f7011eec4b33d7d8f69fd572d3271d588b2cdade
shubham kumar <54357210+shubham4070@users.noreply.github.com>
2023-05-11 21:52:40 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,20 +1,13 @@
-// require the library
-const mongoose= require('mongoose');
-const path= 'mongodb://localhost/issueTracker';
-//  const path = 'mongodb+srv://shubham21101997:1XKDfXnqsQmSfI8C@codeial.0y4o9t7.mongodb.net/?retryWrites=true&w=majority';
-//connected to database -
-mongoose.connect(path);
-
-//acquire the connection to check if it is successful
-const db=mongoose.connection;
-//supress wRNING
+const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-//error
-db.on('error' ,console.error.bind(console,'error connectiong to mongodb'));
-
-//up and running print the message
-db.once('open', function(){
-    console.log('successfully connected to database');
+mongoose.connect('mongodb://localhost:27017/issueTracker', {
+    // useCreateIndex: true,
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    
+    
+}).then(() => {
+    console.log('connection is successfully');
+}).catch((e) => {
+    console.log('No Connection');
 })
-
-module.exports=db;
